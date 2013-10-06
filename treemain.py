0d63fadf0e3c7af5d1e84fbd6ad6d51e4908166f
Takashi Matsuda <mazda.takasky@gmail.com>
2013-10-06 11:26:18 +0900

    parallel 未完成

--- treemain.py
+++ treemain.py
@@ -6,6 +6,7 @@ from solver import transition
 from solver import evaluate_sum
 from solver import possible_area
 from solver import greedy_eval
+from multiprocessing import Pool
 
 N=8
 ENEMYPOWER=4
@@ -31,6 +32,8 @@ def maxroute(e_func,table,color,step):
     """
     pos_area_me=possible_area(table,COLOR)
     print "<calc start on maxroute>"
+
+    
     for i in range(N):
         print str(i)+"/7"
         for j in range(N):
@@ -83,6 +86,12 @@ def best_score(e_func,table,color,step):
     """
     parallel processing below
     """
+    """
+    p=Pool(4)
+    func_args=[(myfunc,a,b) for a in range(N) for b in range(N)]
+    results=p.map(argwrapper,func_args)
+    """
+    
     pos_area_me=possible_area(table,COLOR)
     for i in range(N):
         for j in range(N):
@@ -119,4 +128,33 @@ def best_score(e_func,table,color,step):
     return maxscore
 
 def argwrapper(args):
-    
+    return args[0](*args[1:])
+
+
+def myfunc(e_func,argtable,color,step,a,b):
+     pos_area_me=possible_area(table,COLOR)
+     if pos_area_me[a][b]!=1:
+         pass
+     myturnscore=e_func(a,b,deepcopy(table),COLOR)
+     max_exturn=0
+     exturnscore=0
+     exturn_pos=[0,0]
+     res_for_myturn=transition(deepcopy(table),a,b,COLOR)##
+     pos_area_enemy=possible_area(deepcopy(res_for_myturn),ENEMY)
+     
+     for k in range(N):
+         for l in range(N):
+            if pos_area_enemy[k][l]!=1:
+                continue
+            exturnscore=e_func(k,l,deepcopy(res_for_myturn),ENEMY)
+            if max_exturn < exturnscore:
+                max_exturn=exturnscore
+                exturn_pos=[k,l]
+            if step>0:
+                ## monotomic increase
+                if max_exturn>0:
+                    prediction=myturnscore-max_exturn+best_score(e_func,transition(deepcopy(res_for_myturn),exturn_pos[0],exturn_pos[1],ENEMY),COLOR,step-1)
+                else:
+                    prediction=myturnscore-max_exturn
+            else:
+                prediction=myturnscore-max_exturn
